62a9b854de4261ab300fdd5aed0c060bd4ec4423
KARIM7856 <36071198+KARIM7856@users.noreply.github.com>
2023-10-15 23:47:39 +0300

    .

--- reddit_analysis/wsgi.py
+++ reddit_analysis/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitter_analysis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reddit_analysis.settings')
 
 application = get_wsgi_application()
 
